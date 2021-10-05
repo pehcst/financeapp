722bc6da688eccea510d0217d4f30d1d09c8b20e
Pedro Costa <pehcst@outlook.com.br>
2021-10-05 18:10:49 -0300

    new: summary component and fix global style colors

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import IcomeImg from '../../assets/entradas.svg';
+import OutImg from '../../assets/saidas.svg';
+import TotalImg from '../../assets/total.svg';
+
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={IcomeImg} alt="" />
+        </header>
+        <strong>R$1200,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={OutImg} alt="" />
+        </header>
+        <strong> - R$100,00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={TotalImg} alt="" />
+        </header>
+        <strong>R$1100,00</strong>
+      </div>
+    </Container>
+  )
+}
