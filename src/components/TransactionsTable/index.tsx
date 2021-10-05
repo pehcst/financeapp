71fb827f779c8c319e669a02bbbb7c84a2cd2252
Pedro Costa <pehcst@outlook.com.br>
2021-10-05 18:24:36 -0300

    new: table for finance detail

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styles";
+
+export function TransactionTable() {
+  return(
+  <Container>
+    <table>
+      <thead>
+        <tr>
+          <th>Titulo</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Desenvolvimento de aplicativo</td>
+          <td className="deposit">R$1500,00</td>
+          <td>Freelance</td>
+          <td>01/10/2021</td>
+        </tr>
+        <tr>
+          <td>Pizza</td>
+          <td className="withdraw">- R$120,00</td>
+          <td>Alimentos</td>
+          <td>02/10/2021</td>
+        </tr>
+        </tbody>
+    </table>
+  </Container>
+  )
+}
